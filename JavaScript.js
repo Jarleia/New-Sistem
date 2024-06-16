0da8d17f851a0538ee68eeb592a02831cc2ee3c0
Jarléia Patrícia <166181184+Jarleia@users.noreply.github.com>
2024-06-15 23:28:24 -0300

    JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -20,3 +20,23 @@ function converterParaDolar() {
         alert("Valor inválido. Por favor, digite um número válido.");
     }
 }
+
+function mostrarNumeros() {
+    for (let i = 1; i <= 100; i++) {
+        console.log(i);
+    }
+}
+
+// Chamando a função para mostrar os números de 1 a 100
+mostrarNumeros();
+
+
+function mostrarImparesEntre100e200() {
+    for (let i = 101; i <= 199; i += 2) {
+        console.log(i);
+    }
+}
+
+// Chamando a função para mostrar os números ímpares entre 100 e 200
+mostrarImparesEntre100e200();
+
